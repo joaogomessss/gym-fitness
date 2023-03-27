cc40bbdd22aa22cc4910fd207d7904ca2f01c316
João Gomes <joaossemog@gmail.com>
2023-03-27 19:52:21 -0300

    a

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,20 @@
 
 
 
+Notification.requestPermission()
 
-// check if browser supports notifications
-if ("Notification" in window) {
-    // request permission to show notifications
-    Notification.requestPermission().then(function (permission) {
-    if (permission === "granted") {
-        console.log(permission);
-    // create notification on button click
-    var button = document.getElementById("notification-button");
-    button.addEventListener("click", function() {
-    var notification = new Notification("Hello, World!" , { body:"canguru"    });
-    console.log(notification);
-    
-    });
-    }
-    });
+let button = document.querySelector("#notification-button");
+
+button.onclick = function(){
+
+    new Notification("boi") ;
+ 
+    console.log("showing notification")         
+   
+
+
 }
+
+
+ 
+
